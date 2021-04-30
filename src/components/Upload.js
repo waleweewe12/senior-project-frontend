4622df88fc1a91f24ebffebf2b03a3b1860ab5b1
weerapath ruenrurngdee <m17661weerapast@gmail.com>
2021-05-01 06:18:54 +0700

    add Upload.js

---
+++ src/components/Upload.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Model from './Model';
+import AddImage from './AddImage';
+
+function Upload(){
+    return(
+        <>
+            <AddImage />
+            <Model />
+        </>
+    )
+}
+
+export default Upload;
