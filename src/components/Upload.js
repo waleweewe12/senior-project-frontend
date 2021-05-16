0257e9637027eeba389acafad2cea9def7ba78fd
weerapath ruenrurngdee <m17661weerapast@gmail.com>
2021-05-17 00:18:27 +0700

    add update threshold

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Model from './Model';
 import AddImage from './AddImage';
+import Threshold from './Threshold';
 
 function Upload(){
     return(
         <>
             <AddImage />
             <Model />
+            <Threshold />
         </>
     )
 }
